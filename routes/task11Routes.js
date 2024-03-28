187b281f78bd73fcb98d93dce7ac1c3aab7db09d
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-03-28 11:52:21 +0530

    Added authorization for task11

--- routes/task11Routes.js
+++ routes/task11Routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const passport = require("passport");
 
 const controller = require("../controllers/task11Controllers");
+require("../config/passports")(passport);
 
 const router = express.Router();
 
-router.get("/", controller.showHome);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false, failureRedirect: "/login" }),
+  controller.showHome
+);
 
 module.exports = router;
