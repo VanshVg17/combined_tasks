b6e674495b1764f22c67aae9b176f7d341129ec3
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-04-02 18:55:16 +0530

    Improved structure

--- index.js
+++ index.js
@@ -5,45 +5,18 @@ const passport = require("passport");
 const cookieParser = require("cookie-parser");
 
 require("./config/dbConfig");
-const task16Routes = require("./routes/task16Routes");
-const task1Routes = require("./routes/task1Routes");
-const task2Routes = require("./routes/task2Routes");
-const task3Routes = require("./routes/task3Routes");
-const task4Routes = require("./routes/task4Routes");
-const task5Routes = require("./routes/task5Routes");
-const task6Routes = require("./routes/task6Routes");
-const task7Routes = require("./routes/task7Routes");
-const task8Routes = require("./routes/task8Routes");
-const task9Routes = require("./routes/task9Routes");
-const task10Routes = require("./routes/task10Routes");
-const task11Routes = require("./routes/task11Routes");
-const task12Routes = require("./routes/task12Routes");
-const task13Routes = require("./routes/task13Routes");
+const routes = require("./routes/mainRoutes");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/views"));
 
+app.use(express.static(__dirname + "/views"));
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use("/", task16Routes);
-app.use("/task1", task1Routes);
-app.use("/task2", task2Routes);
-app.use("/task3", task3Routes);
-app.use("/task4", task4Routes);
-app.use("/task5", task5Routes);
-app.use("/task6", task6Routes);
-app.use("/task7", task7Routes);
-app.use("/task8", task8Routes);
-app.use("/task9", task9Routes);
-app.use("/task10", task10Routes);
-app.use("/task11", task11Routes);
-app.use("/task12", task12Routes);
-app.use("/task13", task13Routes);
+app.use("/", routes);
 
 app.listen(process.env.PORT, (error) => {
   if (error) {
---
+++ routes/mainRoutes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const task16Routes = require("./task16Routes");
+const task1Routes = require("./task1Routes");
+const task2Routes = require("./task2Routes");
+const task3Routes = require("./task3Routes");
+const task4Routes = require("./task4Routes");
+const task5Routes = require("./task5Routes");
+const task6Routes = require("./task6Routes");
+const task7Routes = require("./task7Routes");
+const task8Routes = require("./task8Routes");
+const task9Routes = require("./task9Routes");
+const task10Routes = require("./task10Routes");
+const task11Routes = require("./task11Routes");
+const task12Routes = require("./task12Routes");
+const task13Routes = require("./task13Routes");
+
+const router = express.Router();
+
+router.use("/", task16Routes);
+router.use("/task1", task1Routes);
+router.use("/task2", task2Routes);
+router.use("/task3", task3Routes);
+router.use("/task4", task4Routes);
+router.use("/task5", task5Routes);
+router.use("/task6", task6Routes);
+router.use("/task7", task7Routes);
+router.use("/task8", task8Routes);
+router.use("/task9", task9Routes);
+router.use("/task10", task10Routes);
+router.use("/task11", task11Routes);
+router.use("/task12", task12Routes);
+router.use("/task13", task13Routes);
+
+module.exports = router;
