cd803f8a9bc42426a2cf843594f4c08e5a2c20cf
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-04-03 12:14:14 +0530

    Changed the folders name of task1

---
+++ routes/dynamicCellRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const passport = require("passport");
+
+require("../config/passports")(passport);
+const task1Controller = require("../controllers/dynamicCell");
+
+const router = express.Router();
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false, failureRedirect: "/login" }),
+  task1Controller.show
+);
+
+module.exports = router;
