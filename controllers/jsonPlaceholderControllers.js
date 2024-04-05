7abc5849e65eb9e5cb0c7d019baaf2ad02fc18ba
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-04-05 18:14:33 +0530

    Fixed bugs

--- controllers/jsonPlaceholderControllers.js
+++ controllers/jsonPlaceholderControllers.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const posts = (req, res) => {
-  if (req.query.page === undefined) {
+  if (req.query.page === undefined || req.query.page == "") {
     return res.redirect("/task10/posts?page=1&search=none");
   }
   res.sendFile(path.join(__dirname, "../views/jsonPlaceholderViews/posts.html"));
